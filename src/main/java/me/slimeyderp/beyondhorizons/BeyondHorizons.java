9173c404ad2ed3c9744bfc0c70d0312873cc0ad7
Daniel Walsh <walshydev@gmail.com>
2020-04-02 01:38:55 +0100

    Changes

--- src/main/java/me/slimeyderp/beyondhorizons/BeyondHorizons.java
+++ src/main/java/me/slimeyderp/beyondhorizons/BeyondHorizons.java
@@ -13,30 +13,28 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class BeyondHorizons extends JavaPlugin implements SlimefunAddon {
 
+    private static BeyondHorizons instance;
+
     @Override
     public void onEnable() {
+        instance = this;
+
         registerItems();
         addingEnchantments();
         makingAetherWorld();
-        failingToRewriteTheCode();
-        testingCatBazookas();
-        realizingItsABadIdea();
-        hidingEasterEggs();
-        pingingTheBusyBiscuit();
         new SpecialItemListener(this);
         new BukkitUpdater(this, this.getFile(), 370897);
-        getLogger().info("Beyond-Horizons has loaded Sucessfully!");
+        getLogger().info("Beyond-Horizons has loaded Successfully!");
     }
 
     @Override
     public void onDisable() {
-        // Logic for disabling the plugin...
+        instance = null;
     }
 
     @Override
     public String getBugTrackerURL() {
-        // You can return a link to your Bug Tracker instead of null here
-        return null;
+        return "https://github.com/SlimeyDerp/Beyond_Horizons/issues";
     }
 
     @Override
@@ -45,7 +43,6 @@ public class BeyondHorizons extends JavaPlugin implements SlimefunAddon {
     }
 
     private void registerItems() {
-
         Items.ETHERIUM.register(this);
         Items.UNSTABLE_ETHERIUM.register(this);
         Items.ETHERIAL_BALANCE_ROD.register(this);
@@ -64,45 +61,32 @@ public class BeyondHorizons extends JavaPlugin implements SlimefunAddon {
     }
 
     private void addingEnchantments() {
-        ItemMeta ETHERIAL_BALANCE_ROD_META = CustomItemStack.ETHERIAL_BALANCE_ROD_STACK.getItemMeta();
-        ETHERIAL_BALANCE_ROD_META.addEnchant(Enchantment.KNOCKBACK, 5, true);
-        CustomItemStack.ETHERIAL_BALANCE_ROD_STACK.setItemMeta(ETHERIAL_BALANCE_ROD_META);
-        ItemMeta ETHERIAL_PHANTOM_SWORD_META = CustomItemStack.ETHERIAL_PHANTOM_SWORD_STACK.getItemMeta();
-        ETHERIAL_PHANTOM_SWORD_META.addEnchant(Enchantment.DAMAGE_ALL, 5, true);
-        CustomItemStack.ETHERIAL_PHANTOM_SWORD_STACK.setItemMeta(ETHERIAL_PHANTOM_SWORD_META);
-        ItemMeta AETHERIAL_WIND_STAFF_META = CustomItemStack.AETHERIAL_WIND_STAFF_STACK.getItemMeta();
-        AETHERIAL_WIND_STAFF_META.addEnchant(Enchantment.DURABILITY, 1, true);
-        CustomItemStack.AETHERIAL_WIND_STAFF_STACK.setItemMeta(AETHERIAL_WIND_STAFF_META);
-        getLogger().info("Cleaning the dustbins...");
+        ItemMeta etherialBalanceRod = CustomItemStack.ETHERIAL_BALANCE_ROD_STACK.getItemMeta();
+        etherialBalanceRod.addEnchant(Enchantment.KNOCKBACK, 5, true);
+        CustomItemStack.ETHERIAL_BALANCE_ROD_STACK.setItemMeta(etherialBalanceRod);
+
+        ItemMeta etherialPhantomSword = CustomItemStack.ETHERIAL_PHANTOM_SWORD_STACK.getItemMeta();
+        etherialPhantomSword.addEnchant(Enchantment.DAMAGE_ALL, 5, true);
+        CustomItemStack.ETHERIAL_PHANTOM_SWORD_STACK.setItemMeta(etherialPhantomSword);
+
+        ItemMeta aetherialWindStaff = CustomItemStack.AETHERIAL_WIND_STAFF_STACK.getItemMeta();
+        aetherialWindStaff.addEnchant(Enchantment.DURABILITY, 1, true);
+        CustomItemStack.AETHERIAL_WIND_STAFF_STACK.setItemMeta(aetherialWindStaff);
     }
 
     private void makingAetherWorld() {
+        getLogger().info("Rewriting the whole add-on..");
         WorldCreator wc = new WorldCreator("world_aether");
         wc.type(WorldType.NORMAL);
         wc.generator(new CustomChunkGenerator());
         wc.environment(World.Environment.THE_END);
-        wc.createWorld();
-        getLogger().info("Rewriting the whole add-on..");
+        final World aether = wc.createWorld();
+
+        if (aether != null && aether.getBlockAt(0, 1, 0).getType() != Material.END_PORTAL)
+            aether.getBlockAt(0, 1, 0).setType(Material.END_PORTAL);
     }
 
-    private void failingToRewriteTheCode() {
-        Bukkit.getServer().getWorld("world_aether").getBlockAt(0, 1, 0).setType(Material.END_PORTAL);
-        getLogger().info("Failing to re-write the whole add-on...");
-    }
-
-    private void testingCatBazookas() {
-        getLogger().info("Testing Cat Bazookas...");
-    }
-
-    private void realizingItsABadIdea() {
-        getLogger().info("Realizing it's a bad idea...");
-    }
-
-    private void hidingEasterEggs() {
-        getLogger().info("Hiding Easter Eggs...");
-    }
-
-    private void pingingTheBusyBiscuit() {
-        getLogger().info("Pinging TheBusyBiscuit...");
+    public static BeyondHorizons getInstance() {
+        return instance;
     }
 }
--- src/main/java/me/slimeyderp/beyondhorizons/ExtraCategory.java
+++ src/main/java/me/slimeyderp/beyondhorizons/ExtraCategory.java
@@ -5,8 +5,12 @@ import me.mrCookieSlime.Slimefun.cscorelib2.item.CustomItem;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 
-public class ExtraCategory {
-    public static final NamespacedKey CATEGORY_ID = new NamespacedKey(BeyondHorizons.getPlugin(BeyondHorizons.class), "cool_category");
-    public static final CustomItem CATEGORY_ITEM = new CustomItem(Material.HEART_OF_THE_SEA, "Beyond Horizons");
-    public static final Category CUSTOM_CATEGORY = new Category(CATEGORY_ID, CATEGORY_ITEM);
+public final class ExtraCategory {
+
+    public static final Category CUSTOM_CATEGORY = new Category(
+        new NamespacedKey(BeyondHorizons.getInstance(), "beyond_horizons"),
+        new CustomItem(Material.HEART_OF_THE_SEA, "Beyond Horizons")
+    );
+
+    private ExtraCategory() {}
 }
